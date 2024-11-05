48765bea19a23c9b80a606fb3ce23f682148bfe4
chrehall68 <chrehall68@gmail.com>
2024-11-04 19:12:56 -0800

    organize into extension

--- app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Scanner (alexScanTokens)
-
-main :: IO ()
-main = do
-  s <- getContents
-  print (alexScanTokens s)
---
+++ client/src/extension.ts
@@ -0,0 +1,49 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import { ExtensionContext, workspace } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(_: ExtensionContext) {
+  const serverOptions: ServerOptions = {
+    command: "myls",
+    args: [],
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "plaintext" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "languageServerExample",
+    "Language Server Example",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
