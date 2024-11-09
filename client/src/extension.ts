66c77cbe8af468726358c2c29288de34e8cbba0f
rtk0c <mail.tianyig@gmail.com>
2024-11-09 11:01:16 -0800

    Revert "make connection port configurable via env var"
    For some reason EnvironmentVariableCollection contains nothing in the
    extension host. Too much effort to debug now.
    This reverts commit cef8f8883ede8e2b48bfedf6bea150f03eb878f9.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -22,7 +22,7 @@ function getServerOptions(ctx: ExtensionContext): ServerOptions {
     // We need to connect to it with a socket because it's not a child process, no easy way to get its stdin/stdout (on linux, reading /proc/<pid>/0 and 1 is doable, but that's not cross platform) 
     const connectionInfo = {
       host: "localhost",
-      port: parseInt(ctx.environmentVariableCollection.get("VLS_PORT").value),
+      port: 60256,
     };
     return () => {
       const sock = net.connect(connectionInfo);
